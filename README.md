99ecda1624d997ba4031764ba72464456346636d
Loïc Mahieu <mahieuloic@gmail.com>
2018-05-17 00:30:49 +0200

    first draft

---
+++ README.md
@@ -0,0 +1,12 @@
+# create-typescript
+
+A package initialization template for [`typescript`](https://github.com/Microsoft/TypeScript).
+
+```sh
+yarn create typescript
+```
+
+## Inspirations
+
+- [`create-esm`](https://github.com/standard-things/create-esm)
+- [`iarna-create-npm`](https://github.com/iarna/iarna-create-npm)
---
+++ src/create-typescript.ts
@@ -0,0 +1,137 @@
+
+import * as execa from "execa";
+import * as fs from "fs-extra";
+import * as globby from "globby";
+import * as path from "path";
+import * as readPkg from "read-pkg";
+import * as writePkg from "write-pkg";
+
+const dependencies = [
+  "typescript",
+];
+
+const isWin = process.platform === "win32";
+
+const npmBinRegExp = isWin
+  ? /[\\/]np[mx](\.cmd)?$/
+  : /\/np[mx]$/;
+
+const npmJsRegExp = isWin
+  ? /[\\/]node_modules[\\/]npm[\\/]bin[\\/]np[mx]-cli\.js$/
+  : /\/node_modules\/npm\/bin\/np[mx]-cli\.js$/;
+
+export async function install(cwd: string): Promise<void> {
+  const bin = await findBin();
+  await initPackage(bin, cwd);
+  await installDependencies(bin, cwd);
+  await writePackageScripts(cwd);
+  await initFiles(cwd);
+}
+
+async function checkBin(bin: string): Promise<boolean> {
+  return !execa.sync(bin, ["-v"], {
+    reject: false,
+  }).failed;
+}
+
+async function findBin(): Promise<string> {
+  const { env } = process;
+
+  let bin = "yarn";
+
+  if (npmJsRegExp.test(env.NPM_CLI_JS as string) ||
+      npmJsRegExp.test(env.NPX_CLI_JS as string) ||
+      npmBinRegExp.test(env._ as string)) {
+    bin = "npm";
+  }
+
+  if (!await checkBin(bin)) {
+    bin = bin === "yarn" ? "npm" : "yarn";
+
+    if (!await checkBin(bin)) {
+      throw new Error("No package manager found.");
+    }
+  }
+
+  return bin;
+}
+
+async function initPackage(bin: string, cwd: string): Promise<void> {
+  const initArgs = process.argv
+    .slice(2)
+    .filter((arg) => arg.startsWith("-"));
+
+  const binArgs = [
+    "init",
+    ...initArgs,
+  ];
+
+  await execa(bin, binArgs, { cwd, stdio: "inherit" });
+}
+
+async function installDependencies(bin: string, cwd: string): Promise<void> {
+  const args = bin === "yarn"
+    ? ["add", dependencies]
+    : ["i", "--save", dependencies];
+
+  await execa(
+    bin,
+    args as ReadonlyArray<string>,
+    { cwd },
+  );
+}
+
+async function writePackageScripts(cwd: string): Promise<void> {
+  const pkg = await readPkg(cwd);
+  await writePkg(cwd, {
+    ...pkg,
+    files: [
+      "./bin/*",
+      "./lib/*",
+    ],
+    main: "./lib/index.js",
+    typings: "./lib/index.d.ts",
+    scripts: {
+      ...pkg.scripts,
+      build: "tsc",
+      lint: "tslint -c tslint.json src/**/*.ts",
+      prepublish: "npm run build",
+    },
+    _id: undefined,
+    readme: undefined,
+  });
+}
+
+async function initFiles(cwd: string) {
+  const filesFromThisProject = [
+    "tsconfig.json",
+  ];
+  const filesFromTemplate = [
+    ".gitignore",
+    "tslint.json",
+    "README.md",
+    "src/*",
+  ];
+
+  await Promise.all([
+    copyFiles(
+      filesFromThisProject,
+      path.join(__dirname, ".."),
+      cwd,
+    ),
+    copyFiles(
+      filesFromTemplate,
+      path.join(__dirname, "../template"),
+      cwd,
+    ),
+  ]);
+}
+
+async function copyFiles(files: string[], from: string, to: string): Promise<void> {
+  const expandedFiles = await globby(files, { cwd: from });
+
+  await Promise.all(expandedFiles.map(async (file) => {
+    await fs.mkdirp(path.dirname(path.join(to, file)));
+    await fs.copy(path.join(from, file), path.join(to, file));
+  }));
+}
